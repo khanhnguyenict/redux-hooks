89eed46632c5f84a4e7a58a7735643f17abe06ba
test <khanhnguyen.ict@gmail.com>
2020-01-06 14:58:13 +0700

    call api with effect

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,42 +1,23 @@
 import {
-    ADD_TODO,
-    TOGGLE_TODO,
-    SET_FILTER,
     ADD_USER,
+    SUB_USER,
     ADD_ARTICLE
 } from "./actionTypes";
 
-
-let nextTodoId = 0;
-
-export const addTodo = content => ({
-    type: ADD_TODO,
-    payload: {
-        id: ++nextTodoId,
-        content
-    }
-});
-
-export const toggleTodo = id => ({
-    type: TOGGLE_TODO,
-    payload: {
-        id
-    }
-});
-export const setFilter = filter => ({
-    type: SET_FILTER,
-    payload: {
-        filter
-    }
-})
-//
 
 let userId = 0;
 export const addUser = name => ({
     type: ADD_USER,
     payload: {
-        id: ++userId,
-        name: `User ${userId}`
+        id: ++ userId,
+        name
+    }
+});
+export const subUser = id => ({
+    type: SUB_USER,
+    payload: {
+        id: id,
+        // name: `User ${userId}`
     }
 });
 //
@@ -44,7 +25,15 @@ let articleId = 0;
 export const addArticle = name => ({
     type: ADD_ARTICLE,
     payload: {
-        id: ++articleId,
+        id: ++ articleId,
+        name: name
+    }
+});
+
+export const subArticle = name => ({
+    type: ADD_ARTICLE,
+    payload: {
+        id: --articleId,
         name: `Article ${articleId}`
     }
 });
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,17 +3,36 @@ import {
 } from "redux";
 import users from './users';
 import articles from './articles';
+import downloads from "./downloads";
 // higher order reducer
 const addReducer = (reducer, section) => (state, action) => {
-    if (action.type === `ADD_${section}`) {
-        return [...state, action.payload]
+    const { type, payload } = action;
+    if (type === `ADD_${section}`) {
+        return [...state, payload]
     }
+    if (type === `SUB_${section}`) {
+        console.log('state1: ', state)
+        const data = [...state.filter(item => item.id !== payload.id)];
+        console.log('data:', data);
+          return data;
+      }
     return reducer(state, action);
 }
 
+const getReducer = (reducer, section) => (state, action) => {
+    const { type, payload } = action;
+    if (type === `GET_${section}`) {
+        return [...state, payload]
+    }
+   
+    return reducer(state, action);
+}
+
+
 const rootReducer = combineReducers({
     users: addReducer(users, 'USER'),
-    articles: addReducer(articles, 'ARTICLE')
+    articles: addReducer(articles, 'ARTICLE'),
+    libraries : getReducer(downloads, '')
 
 })
 export default rootReducer;
