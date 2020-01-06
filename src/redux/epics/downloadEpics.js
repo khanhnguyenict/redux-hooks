e8e2e11047bde3a0bbd868bdf6fcc4a519513b75
test <khanhnguyen.ict@gmail.com>
2020-01-06 15:39:39 +0700

    call api

--- src/redux/epics/downloadEpics.js
+++ src/redux/epics/downloadEpics.js
@@ -1,25 +1,30 @@
 import {
   switchMap,
-  map
 } from 'rxjs/operators';
 import {
   ofType
 } from 'redux-observable';
 import {
-  GET_DOWNLOAD
+  GET_DOWNLOAD,
 } from "../actions/actionTypes";
 import * as service from '../service';
 import {
   getDownloadSuccess
 } from '../actions/downloadAction';
 
+import 'rxjs';
+ import axios from 'axios';
+ import { from } from 'rxjs';
+ import { mergeMap, map } from 'rxjs/operators';
 
 
-const countEpic = (action$, state$) => action$.pipe(
+ export const downloadEpic = action$ =>
+   action$.pipe(
   ofType(GET_DOWNLOAD),
-  switchMap(() => service.getDownload().pipe(
-    map(response => getDownloadSuccess(response))
-  ))
-);
+  switchMap(action =>
+     from(axios.get('http://www.mocky.io/v2/5e0eb33634000090002d7c9c')).pipe(
+      map(response => getDownloadSuccess(response))
+     ))
+)
 
-export default countEpic;
+export default downloadEpic;
