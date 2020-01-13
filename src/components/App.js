3d69d02dabce702fc94c511a8a0c6ac5a366195b
test <khanhnguyen.ict@gmail.com>
2020-01-13 16:42:49 +0700

    apply useReducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,19 @@
 import * as React from "react";
-import { getDownload } from "./../redux/actions/downloadAction";
-import { useSelector, useDispatch, shallowEqual } from "react-redux";
-import { useEffect } from "react";
-// import Login from './Login';
-import { Table } from "antd";
-import "antd/dist/antd.css";
-import { Spin } from "antd";
-
-const { Column } = Table;
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from './Login';
+import User from './User';
 
 const App = () => {
-  // use dispatch
-  const dispatch = useDispatch();
-  const { downloads = [] } = useSelector(
-    state => ({
-      users: state.users,
-      downloads: state.downloadLibraryInfo.data.downloads
-    }),
-    shallowEqual
-  );
-  const countTotalDownload = (downloads = []) =>
-    downloads.reduce((total, currentItem) => total + currentItem.downloads, 0);
-
-  useEffect(() => {
-    dispatch(getDownload());
-  }, []);
-
   return (
-    <>
-      <h1>Total downloads : {countTotalDownload(downloads)}</h1>
-      {downloads.length && (
-        <Table dataSource={downloads}>
-          <Column title="Total per day" dataIndex="downloads" key="downloads" />
-          <Column title="Day" dataIndex="day" key="day" />
-        </Table>
-      )}
-      {!downloads.length && <Spin />}
-    </>
+    <Router>
+      <div>
+        <h1>Test</h1> 
+        <Route component={Login} path="/login" exact /> 
+        <Route component={User} path="/user" /> 
+
+        {/* <Route component={About} path="/about" />  */}
+      </div>
+    </Router>
   );
 };
 
---
+++ src/components/User.js
@@ -0,0 +1,53 @@
+import * as React from "react";
+import { useRef, useReducer } from "react";
+
+export default function User() {
+  const inputRef = useRef();
+  const [items, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case "add":
+          return [...state, {
+              id : state.length,
+              name : action.name
+          }]
+        case 'remove':
+            const data = state.filter(item => item.id !== action.id);
+            return data;
+      default:
+
+        return state;
+    }
+  }, []);
+
+  function handleSumit(e) {
+    e.preventDefault();
+    dispatch({
+      type: "add",
+      name: inputRef.current.value
+    });
+    inputRef.current.value = "";
+  }
+  function removeItem(id) {
+    // e.preventDefault();
+    dispatch({
+      type: "remove",
+      id
+    });
+    inputRef.current.value = "";
+  }
+  return (
+    <>
+      <form onSubmit={handleSumit}>
+        <input ref={inputRef} />
+      </form>
+      <ul>
+        {items.map(item => (
+            <div key = {item.id}>
+          <li>{item.name}</li>
+          <button  onClick={()=> removeItem(item.id)}> Remove item</button>
+          </div>
+        ))}
+      </ul>
+    </>
+  );
+}
